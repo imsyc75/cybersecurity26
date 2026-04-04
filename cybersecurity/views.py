eee274bd5a1f83d27594e7c277a4ec63d4ed387b
yuchen <songyuchen2003@hotmail.com>
2026-04-04 19:00:43 +0300

    flaw2

--- cybersecurity/views.py
+++ cybersecurity/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Note
 from django.http import HttpResponseForbidden
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.db import connection
 
 @login_required
 def view_note(request, note_id):
-    # flaw: Notes were accessed directly by ID without checking ownership.
+    # flaw1: Notes were accessed directly by ID without checking ownership.
     # Attackers can view other people's notes simply by modifying the ID in the URL.
     note = get_object_or_404(Note, pk=note_id)
 
-    # fix: Check if the owner of the note is the user who made the current request.
+    # fix1: Check if the owner of the note is the user who made the current request.
     # note = get_object_or_404(Note, pk=note_id)
     # if note.user != request.user:
     #     return HttpResponseForbidden("<h1>403 Forbidden</h1><p>You do not have permission to view this note.</p>")
@@ -21,3 +20,27 @@ def view_note(request, note_id):
 def home(request):
     user_notes = Note.objects.filter(user=request.user)
     return render(request, 'home.html', {'notes': user_notes})
+
+
+@login_required
+def search_notes(request):
+    query = request.GET.get('query', '')
+    notes = []
+    
+    if query:
+        # flaw2：Directly concatenating user input using f-string or string addition
+        # This method does not escape user input, leading to SQL injection.
+        sql = f"SELECT * FROM cybersecurity_note WHERE title = '{query}'"
+        
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+            columns = [col[0] for col in cursor.description]
+            notes = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    
+        # fix2: Use parameterized queries to prevent SQL injection.
+        # sql = "SELECT * FROM cybersecurity_note WHERE title = %s"
+        # with connection.cursor() as cursor:
+        #     cursor.execute(sql, [query])
+        #     columns = [col[0] for col in cursor.description]
+        #     notes = [dict(zip(columns, row)) for row in cursor.fetchall()]        
+    return render(request, 'search.html', {'notes': notes, 'query': query})
--- myproject/urls.py
+++ myproject/urls.py
@@ -22,6 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('note/<int:note_id>/', views.view_note, name='view_note'),
+    path('search/', views.search_notes, name='search_notes'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
